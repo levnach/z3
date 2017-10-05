c5fa268f235a1cab370b1cc0f2d0374809705bbe
Lev Nachmanson <levnach@hotmail.com>
2017-10-05 10:34:59 -0700

    vector bug
    Signed-off-by: Lev Nachmanson <levnach@hotmail.com>

--- src/test/lp/lp.cpp
+++ src/test/lp/lp.cpp
@@ -3178,14 +3178,12 @@ void test_vector() {
 void test_cut_solver() {
     ccc cs;
     vector<rational> term;
-    for (int i = 0;i  < 7; i++)
+    for (int i = 0; i  < 7; i++)
         cs.add_var();
 
     term.push_back(rational(2));
     term.push_back(rational(2));
     cs.add_ineq(term);
-    cs.add_ineq(term);
-    term.push_back(rational(2));
     cs.add_ineq(term);
 }
 
